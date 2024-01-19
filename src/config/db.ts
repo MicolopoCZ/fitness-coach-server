9954e1da89b6377a4ba0719337c8092ec2a45de8
Michal <armymisa@gmail.com>
2024-01-19 12:03:40 +0100

    xampp gaming

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,6 +1,5 @@
 import { Dialect } from "sequelize";
 
-
 export const dbConfig = {
     HOST: process.env.DB_HOST as string,
     USER: process.env.DB_USER as string,
@@ -8,4 +7,10 @@ export const dbConfig = {
     DB: process.env.DB_NAME as string,
     PORT: process.env.DB_PORT as unknown as number,
     dialect: "mysql" as Dialect,
-}
+    pool: {
+        max: 5,
+        min: 0,
+        acquire: 30000,
+        idle: 10000,
+    },
+};
--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import dotenv from "dotenv";
 dotenv.config(); // načte .env soubor do process.env
 //process.env.PORT
 import cors = require("cors");
+import db from "./models/index";
 
 const app = express();
 app.use(express.json());
@@ -10,8 +11,10 @@ app.use(cors());
 
 const PORT = process.env.PORT || 3000;
 
+db.sequelize.sync({ force: true, alter: true });
+
 app.use(`/api/v${process.env.API_VER}/user`, require("./routes/user"));
 
 app.listen(PORT, () => {
     console.log(`App is running on ${PORT}`)
-})
+});
